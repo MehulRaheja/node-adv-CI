e0760d3f14c8cacd75128b6d151bde284436429a
mehuledneed <mehul.edneed@gmail.com>
2023-01-31 12:41:22 +0530

    check

--- AdvancedNodeStarter/tests/helpers/page.js
+++ AdvancedNodeStarter/tests/helpers/page.js
@@ -59,7 +59,7 @@ class CustomPage {
         method: 'GET',
         credentials: 'same-origin',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json' 
         }
       }).then(res => res.json());
     }, path);
