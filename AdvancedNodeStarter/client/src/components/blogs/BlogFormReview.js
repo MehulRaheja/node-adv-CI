d14af7eceb87a69581e1e7b7b9891e2c940b74da
mehuledneed <mehul.edneed@gmail.com>
2023-02-03 17:22:41 +0530

    done with the course

--- AdvancedNodeStarter/client/src/components/blogs/BlogFormReview.js
+++ AdvancedNodeStarter/client/src/components/blogs/BlogFormReview.js
@@ -7,6 +7,8 @@ import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
 
 class BlogFormReview extends Component {
+  state = { file: null };
+
   renderFields() {
     const { formValues } = this.props;
 
@@ -39,12 +41,17 @@ class BlogFormReview extends Component {
     );
   }
 
+  // code added here
   onSubmit(event) {
     event.preventDefault();
 
     const { submitBlog, history, formValues } = this.props;
 
-    submitBlog(formValues, history);
+    submitBlog(formValues, this.state.file, history);
+  }
+
+  onFileChange(event) {
+    this.setState({ file: event.target.files[0] });
   }
 
   render() {
@@ -52,6 +59,14 @@ class BlogFormReview extends Component {
       <form onSubmit={this.onSubmit.bind(this)}>
         <h5>Please confirm your entries</h5>
         {this.renderFields()}
+
+        <h5>Add An Image</h5>
+        { /*use double quotes here, with accept attribute we will see only image files while selecting */}
+        <input
+          onChange={this.onFileChange.bind(this)}
+          type="file"
+          accept="image/*"
+        /> 
 
         {this.renderButtons()}
       </form>
